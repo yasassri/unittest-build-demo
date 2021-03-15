cfb4b3debeb01836a23d292c63a1d2dbe45240df
Yasassri <ycrnet@gmail.com>
2021-03-15 21:17:37 +0530

    Add build.sh

---
+++ resources/scripts/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+##
+# This script takes care of build process of the artiffacts
+##
+
+WORKSPACE="/workspace"
+SOURCE_LOCATION="/workspace"
+
+# Functions
+function echoBold () {
+    echo -e $'\e[1m'"${1}"$'\e[0m'
+}
+
+########################
+##   Main Execution   ##
+########################
+
+# Check the welformedness of the maas yaml
+echo "Start Execution!!!"
+echo $WSO2_SERVER_HOME
+
+# Build with Tests
+cd $SOURCE_LOCATION
+mvn clean install -DtestServerType=local -DtestServerPath=$WSO2_SERVER_HOME/bin/micro-integrator.sh
